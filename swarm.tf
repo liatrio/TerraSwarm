9bc5cb02b7e7016fbb69f03cfaf4c7f551c8dd20
HunterMayers <huntercmayers@gmail.com>
2017-07-26 17:20:50 -0700

    Tightened security groups slightly

--- swarm.tf
+++ swarm.tf
@@ -135,7 +135,6 @@ resource "aws_security_group" "swarm_internal" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
     self        = "true"
   }
 }
